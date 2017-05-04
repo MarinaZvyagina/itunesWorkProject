3b62d8d13a3b5254595844c765e3f26828bd46cf
Марина Звягина <zvyagina.marina@gmail.com>
2017-05-04 20:55:30 +0300

    Initial Commit

---
+++ itunesWorkProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  itunesWorkProject
+//
+//  Created by Марина Звягина on 04.05.17.
+//  Copyright © 2017 Zvyagina Marina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ itunesWorkProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  itunesWorkProject
+//
+//  Created by Марина Звягина on 04.05.17.
+//  Copyright © 2017 Zvyagina Marina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
