3fc8f84fd7064b4ae4c9c273b1819e38ee202923
Марина Звягина <zvyagina.marina@gmail.com>
2017-06-03 01:04:12 +0300

    Fixed work with artists names consists of more then one word

--- itunesWorkProject/IWPNetworkDataBase.m
+++ itunesWorkProject/IWPNetworkDataBase.m
@@ -11,21 +11,35 @@
 
 @implementation IWPNetworkDataBase
 
--(IWPSongList *)getSongs: (NSString *)artist withUpdate: (IWPSongList *)songs  {
+-(NSString *)addPlusBetweenWords: (NSString *)artist {
     if (artist == nil )
         artist = @"";
+    
+    NSArray <NSString *> *terms = [NSArray new];
+    terms = [artist componentsSeparatedByString:@" "];
+    NSString * artistString = [NSString new];
+    
+    for (NSString *term in terms) {
+        if (term.length >= 1) {
+            artistString = [artistString stringByAppendingString:term];
+            artistString = [artistString stringByAppendingString:@"+"];
+        }
+    }
+    if (artistString.length > 0) {
+        artistString = [artistString substringToIndex:artistString.length - 1];
+    }
+    return artistString;
+}
+
+-(IWPSongList *)getSongs: (NSString *)artist withUpdate: (IWPSongList *)songs  {
+    NSString * artistString = [self addPlusBetweenWords:artist];
     __block NSMutableArray *resultSongs = [NSMutableArray new];
     NSString * stringWithoutArtist = @"https://itunes.apple.com/search?term=";
-    NSString * stringWithArtist = [stringWithoutArtist stringByAppendingString:artist];
-
+    NSString * stringWithArtist = [stringWithoutArtist stringByAppendingString:artistString];
     NSURLRequest *nsurlRequest=[NSURLRequest requestWithURL:[NSURL URLWithString:stringWithArtist]];
-    
     __block NSData *responseData = [NSURLConnection sendSynchronousRequest:nsurlRequest returningResponse:nil error:nil];
-    
     NSURLSessionConfiguration * defaultConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:defaultConfiguration delegate:self delegateQueue:[NSOperationQueue mainQueue]];
-    
-    
     [[session dataTaskWithRequest:nsurlRequest
                 completionHandler:^(NSData *data,
                                     NSURLResponse *response,
@@ -33,6 +47,9 @@
                     responseData = data;
                 }] resume];
   
+    if ( responseData == nil ) {
+        return [IWPSongList new];
+    }
     NSDictionary * JSONObject = [NSJSONSerialization JSONObjectWithData:responseData options:0 error:nil];
      
     IWPSong* (^createSong)(NSString *, NSString *, NSString *, NSString *, NSString *);
